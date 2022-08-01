889b71d3d0e967c181e2bc8e0df7259940f5408d
DayanneAyres <dayannefayres@gmail.com>
2022-08-01 15:48:28 -0300

    Fazendo a leitura assincrona com promises.

--- NodeJS_Criando_Biblioteca/lib-markdown/index.js
+++ NodeJS_Criando_Biblioteca/lib-markdown/index.js
@@ -1,13 +1,29 @@
 import chalk from 'chalk';
 import fs from 'fs';
 
+function trataErro(erro){
+    throw new Error(chalk.red(erro.code, 'não há arquivo no caminho'));
+}
+
 function pegaArquivo(caminhoDoArquivo){
+    const enconding = 'utf-8';
+    // lendo arquivo de forma assíncrona
+    fs.promises
+    .readFile(caminhoDoArquivo, enconding)
+    .then(texto => console.log(chalk.green(texto)))
+    .catch(erro => trataErro(erro));
+}
+
+/* function pegaArquivo(caminhoDoArquivo){
     
     const enconding = 'utf-8';
     //fs.readFile(filePath(string), enconding, callback function(parametros: erro, dados))
-    fs.readFile(caminhoDoArquivo, enconding, (_, texto) => {
+    fs.readFile(caminhoDoArquivo, enconding, (erro, texto) => {
+        if(erro){
+            trataErro(erro);
+        }
         console.log(chalk.green(texto));
     })
-}
+} */
 
-pegaArquivo('./arquivos/texto1.md')
+pegaArquivo('./arquivos/texto1.md');
