dd1b4d55fd2ecfedfb3a0d9507daa5c45f8dcb9a
DayanneAyres <dayannefayres@gmail.com>
2022-08-01 11:13:40 -0300

    Destacando texto.

---
+++ NodeJS_Criando_Biblioteca/lib-markdown/index.js
@@ -0,0 +1,10 @@
+import chalk from 'chalk';
+
+// destaca o texto, colocando a cor azul
+console.log(chalk.blue('Vamos começar!'));
+
+const paragrafo = 'Texto retornado por uma funcao.';
+
+const texto = string => string;
+
+console.log(texto(paragrafo));
