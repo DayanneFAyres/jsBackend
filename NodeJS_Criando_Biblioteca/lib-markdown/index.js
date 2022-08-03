081890b199d3c0301cf7a15872abe0503100993d
Dayanne Ayres <dayannefayres@gmail.com>
2022-08-03 18:12:23 -0300

    Identificando links com regex.

--- NodeJS_Criando_Biblioteca/lib-markdown/index.js
+++ NodeJS_Criando_Biblioteca/lib-markdown/index.js
@@ -1,6 +1,31 @@
 import chalk from 'chalk';
 import fs from 'fs';
 
+function extraiLinks(texto){
+    /* 
+    é necessario incluir / no inicio e no final do regex, com gm no final para que possa reconhecer o padrao.
+    gm significa global e com multilinhas, para que nao pare no primeiro match e nem com quebra de linha.
+
+    classes sao representados por colchetes [abc]
+    grupos sao representados por parenteses ([a-z][12])
+    o circunflexo representa negaçao, sendo o caractere que nao está buscando ^#
+    a interrogação representa um caractere opcional, é colocada depois dele. s?
+    para incluir um caractere especial que faz parte do padrao regex, coloca-se a \ antes. \(
+    O asterisco representa multiplas ocorrencias do padrao em questao. [a-z]*
+    */
+    const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s]*.[^\s]*)\)/gm;
+    // match é um metodo de string, que retorna todo link sem separar os grupos
+    //const linksExtraidos = texto.match(regex);
+    const arrayResultados = [];
+    let temp;
+    while((temp = regex.exec(texto)) !== null){
+        arrayResultados.push({[temp[1]]:temp[2]});
+    } 
+       
+    console.log(arrayResultados);
+
+}
+
 function trataErro(erro){
     throw new Error(chalk.red(erro.code, 'não há arquivo no caminho'));
 }
@@ -11,8 +36,8 @@ async function pegaArquivo(caminhoDoArquivo){
     const encoding = 'utf-8';
 
     try{
-        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
-        console.log(chalk.green(texto))
+        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
+        extraiLinks(texto);
     }
     catch(erro){
         trataErro(erro);
@@ -41,4 +66,4 @@ async function pegaArquivo(caminhoDoArquivo){
     })
 } */
 
-pegaArquivo('./arquivos/texto1.md');
+pegaArquivo('.\\arquivos\\texto1.md');
